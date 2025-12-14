b5bd6188ac190a9a2d6a1b73f8633c4302a75da9
Akinbobola <faniyiakinbobola@gmail.com>
2025-12-14 01:09:51 +0100

    chore: remove dist/ build artifacts from version control and update .gitignore

--- dist/apps/api-gateway/libs/common/src/utils/sku.util.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateSku = generateSku;
-function generateSku() {
-    const random = Math.random().toString(36).substring(2, 8).toUpperCase();
-    const timestamp = Date.now().toString().slice(-6);
-    return `SKU-${random}-${timestamp}`;
-}
-//# sourceMappingURL=sku.util.js.map
--- dist/apps/notifications/libs/common/src/dtos/update-user-address.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateUserAddressDto = void 0;
-const mapped_types_1 = require("@nestjs/mapped-types");
-const create_user_address_dto_1 = require("./create-user-address.dto");
-class UpdateUserAddressDto extends (0, mapped_types_1.PartialType)(create_user_address_dto_1.CreateUserAddressDto) {
-}
-exports.UpdateUserAddressDto = UpdateUserAddressDto;
-//# sourceMappingURL=update-user-address.dto.js.map
